157336e889ccccc13ccaee27da932b9b201c045b
test <sebastian.kuppa@web.de>
2022-12-05 17:11:15 +0800

    added App class for programm frame

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import tkinter
 
+
+# creating calc frame
+def iCalc(source, side):
+    storeObj = tkinter.Frame()
+
+
+class App(tkinter.Frame):
+    def __init__(self):
+        tkinter.Frame.__init__(self)
+        self.option_add('*Font', 'arial 20 bold')
+        self.pack(expand=True, fill=tkinter.BOTH)
+        self.master.title('Calculator')
+
+
 if __name__ == '__main__':
-    app().mainloop()
+    App().mainloop()
